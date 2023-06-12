4554342030263676c01a74298e1c23a216856c28
An Tran <antran1245@gmail.com>
2023-06-11 21:05:07 -0700

    making a group buttons for next and previous question

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@
 import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 import Question from '@/components/Question'
+import arrow_left from '/public/assets/left_arrow.svg'
+import arrow_right from '/public/assets/right_arrow.svg'
+import Image from 'next/image'
+
 
 export default function Home() {
   const [questions, setQuestions] = useState<any[]>([])
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=1")
+    fetch("https://opentdb.com/api.php?amount=2")
     .then(resp => resp.json())
     .then(data => {
       setQuestions(data.results)
@@ -17,6 +21,10 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Question question={questions[0]}/>
+      <div>
+        <Image src={arrow_left} alt='arrow left'/>
+        <Image src={arrow_right} alt='arrow right'/>
+      </div>
     </main>
   )
 }
