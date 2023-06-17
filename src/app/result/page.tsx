3cee219845bc96c3e4d88ecefb6334c94888808e
An Tran <antran1245@gmail.com>
2023-06-17 15:20:36 -0700

    creating the result page

---
+++ src/app/result/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+export default function Result() {
+    return (
+        <main className={styles.main}>
+            <h1>hello</h1>
+        </main>
+    )
+}
