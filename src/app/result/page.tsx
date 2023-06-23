41491ee0beace420e310a834c6742d4d8c6276d5
An Tran <antran1245@gmail.com>
2023-06-23 09:19:46 -0700

    display question content

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -6,8 +6,17 @@ import styles from '../page.module.css'
 export default function Result() {
     const context = useContext(Contexts)
     return (
-        <main className={styles.main}>
+        <main className={`${styles.main} ${styles.result}`}>
             <h1>Score: {context?.total}</h1>
+            <div>
+                {context?.questions.map((item : {[key: string] : any}, index : number) => {
+                    return <div key={index}>
+                            <p>Question {index+1}: {item.question.replaceAll('&quot;', `"`)}</p>
+                            <p>Correct Answer: {item.correct_answer}</p>
+                            <p>Your Answer: {context?.answers[index]}</p>
+                        </div>
+                })}
+            </div>
         </main>
     )
 }
