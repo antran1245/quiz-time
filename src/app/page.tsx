9d6db5b3f21668fd3eb0a07f4d74b6f72e669a70
An Tran <antran1245@gmail.com>
2023-06-28 17:25:17 -0700

    transfer the content of '/' page.tsx to '/quiz' page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,12 @@
 "use client"
-import { useContext, useEffect, useState } from 'react'
 import styles from './page.module.css'
-import Question from '@/components/Question'
-import Sidebar from '@/components/Sidebar'
-import { Contexts } from '@/contexts/context'
 
 
 export default function Home() {
-  const [questions, setQuestions] = useState<any[]>([])
-  const [curr, setCurr] = useState<number>(0)
-  const context = useContext(Contexts)
-
-  useEffect(() => {
-    if(questions.length === 0) {
-      fetch("https://opentdb.com/api.php?amount=2")
-      .then(resp => resp.json())
-      .then(data => {
-        context?.setAnswers(new Array(data.results.length))
-        context?.setQuestions(data.results)
-        setQuestions(data.results)
-      })
-      .catch(err => console.log(err))
-    }
-  }, [])
 
   return (
     <main className={styles.main}>
-      <Question questions={questions} curr={curr} setCurr={setCurr}/>
-      <Sidebar questions={questions} setCurr={setCurr}/>
+      <form></form>
     </main>
   )
 }
